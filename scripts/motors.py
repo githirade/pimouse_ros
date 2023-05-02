a383e8516fe7e1f61a1354a1745d253ca9becc29
githirade <takahiro_hirade@aichi-inst.jp>
2023-05-02 05:02:19 +0000

    intend err

--- scripts/motors.py
+++ scripts/motors.py
@@ -73,11 +73,11 @@ class Motor():
             with open(dev,'w') as f:
                 f.write("%d %d %d\n" %
                         (message.left_hz,message.right_hz,message.duration_ms))
-            except:
-                rospy.logerr("cannot write to " + dev)
-                return False
+        except:
+            rospy.logerr("cannot write to " + dev)
+            return False
 
-            return True
+        return True
 
 if __name__ == '__main__':
     rospy.init_node('motors')
