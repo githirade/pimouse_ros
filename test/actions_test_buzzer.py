4e157f2ddf50334dc9b1a0940c4d94991f37ee98
githirade <takahiro_hirade@aichi-inst.jp>
2023-05-02 08:18:55 +0000

    simple to Simple

--- test/actions_test_buzzer.py
+++ test/actions_test_buzzer.py
@@ -9,7 +9,7 @@ from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal
 
 class BuzzerTest(unittest.TestCase):
     def setUp(self):
-        self.client = actionlib.simpleActionClient("music", MusicAction)
+        self.client = actionlib.SimpleActionClient("music", MusicAction)
         self.device_values = []
 
     def test_node_exist(self):
